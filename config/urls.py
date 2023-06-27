4f98d4b9d9bfba2c816d694a2684c8b1bcee17ff
valla <kochemarov@gmail.com>
2023-06-27 15:20:43 +0600

    created category

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/accounts/', include('account.urls'))
+    path('api/v1/accounts/', include('account.urls')),
+    path('api/v1/category/', include('category.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
